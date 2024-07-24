c91235ad6c2ffe7a0348802396aafd70e3a35663
mfloresz <mfloresz@protonmail.com>
2024-07-24 13:35:24 -0600

    fix varable names

--- install.sh
+++ install.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
 # Define las rutas
-path_to_s="$HOME/.local/share/kio/servicemenus/scripts"
-path_to_d="$HOME/.local/share/kio/servicemenus"
+path_to_sf="$HOME/.local/share/kio/servicemenus/scripts"
+path_to_df="$HOME/.local/share/kio/servicemenus"
 script_source="$(dirname "${BASH_SOURCE[0]}")"
 
 # Crear el directorio de path_to si no existe
-mkdir -p "$path_to_s"
-mkdir -p "$path_to_d"
+mkdir -p "$path_to_sf"
+mkdir -p "$path_to_df"
 
 # Copiar los archivos
 # Desktop file
-cp "$script_source/For KDE/ServiceMenu/tools.desktop" "$path_to_d/"
+cp "$script_source/For KDE/ServiceMenu/tools.desktop" "$path_to_df/"
 # Scripts
-cp "$script_source/For KDE/ServiceMenu/scripts/move_to_folder.sh" "$path_to_s/"
-cp "$script_source/For KDE/ServiceMenu/scripts/ren_win.sh" "$path_to_s/"
-cp "$script_source/For KDE/ServiceMenu/scripts/ts_to_mp4.sh" "$path_to_s/"
+cp "$script_source/For KDE/ServiceMenu/scripts/move_to_folder.sh" "$path_to_sf/"
+cp "$script_source/For KDE/ServiceMenu/scripts/ren_win.sh" "$path_to_sf/"
+cp "$script_source/For KDE/ServiceMenu/scripts/ts_to_mp4.sh" "$path_to_sf/"
 
 # Cambiar propietario y grupo al usuario actual
-chown $USER:$USER "$path_to_s/move_to_folder.sh"
-chown $USER:$USER "$path_to_s/ren_win.sh"
-chown $USER:$USER "$path_to_s/ts_to_mp4.sh"
+chown $USER:$USER "$path_to_sf/move_to_folder.sh"
+chown $USER:$USER "$path_to_sf/ren_win.sh"
+chown $USER:$USER "$path_to_sf/ts_to_mp4.sh"
+chown $USER:$USER "$path_to_df/tools.desktop"
 
 # Asignar permisos de ejecución al script
-chmod +x "$path_to_s/move_to_folder.sh"
-chmod +x "$path_to_s/ren_win.sh"
-chmod +x "$path_to_s/ts_to_mp4.sh"
+chmod +x "$path_to_sf/move_to_folder.sh"
+chmod +x "$path_to_sf/ren_win.sh"
+chmod +x "$path_to_sf/ts_to_mp4.sh"
+chmod +x "$path_to_df/tools.desktop"
 
 # Mensaje de éxito
 kdialog --title "Instalación completa" --passivepopup "Los archivos han sido copiados correctamente" 10
